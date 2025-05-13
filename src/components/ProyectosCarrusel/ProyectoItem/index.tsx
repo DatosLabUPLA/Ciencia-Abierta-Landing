04a2e0d43bd5d015b12654da0063771b9f770c09
SAGITOR <marco.vivar1205@gmail.com>
2025-05-13 11:54:11 -0400

    home page responsive

--- src/components/ProyectosCarrusel/ProyectoItem/index.tsx
+++ src/components/ProyectosCarrusel/ProyectoItem/index.tsx
@@ -12,30 +12,27 @@ const ProyectoItem = ({
     description,
     currentPosition
 }: IProyectoCarrusel) => {
-    const [transform, setTransform] = useState({
+    const [transformStyles, setTransformStyles] = useState({
         transform: "translateX(0)"
     })
     
     useEffect(() => {
         //OPTIMIZAR
         const transformStyle = {
-            transform: ""
-        }
-        if (position < currentPosition) {
-            transformStyle.transform = "translateX(-120vw)"
-        } else if (position > currentPosition) {
-            transformStyle.transform = "translateX(120vw)"
-        } else {
-            transformStyle.transform = "translateX(0)"
+            transform: "translateX(0)"
         }
 
-        setTransform(prevState => {
+        if (position !== currentPosition) {
+            transformStyle.transform = position < currentPosition ? "translateX(-120vw)" : "translateX(120vw)";
+        }
+
+        setTransformStyles(prevState => {
             return {...prevState, ...transformStyle}
         });
     }, [currentPosition]);
 
     return (
-        <div className = {styles["proyecto-main__container"]} style = {transform}>
+        <div className = {styles["proyecto-main__container"]} style = {transformStyles}>
             <p className = {styles["proyecto__text"]}>{description}</p>
             <h1 className = {styles["proyecto__title"]}>{name}</h1>
             <img className = {styles["proyecto__image"]} src = {imagePath}/>
